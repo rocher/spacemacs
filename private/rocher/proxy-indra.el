17600289520d001602e8ad2e5513c9a6826d67ed
Francesc Rocher <francesc.rocher@gmail.com>
2017-10-04 15:43:08 +0200

    proxy services updated with 'no_proxy' entries

--- private/rocher/proxy-indra.el
+++ private/rocher/proxy-indra.el
@@ -1,4 +1,4 @@
 (setq url-proxy-services
       '(("http" . "proxy.indra.es:8080")
-        ("https" . "proxy.indra.es:8080")))
-(setenv "no_proxy" "localhost,.indra.es")
+        ("https" . "proxy.indra.es:8080")
+        ("no_proxy" . "\\(localhost\\|\\.indra\\.es\\|.dev\\)")))
